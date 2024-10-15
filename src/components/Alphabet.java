5ad5bf919c92f517280e11cd46307c2198e3e06d
Sergio Nicolas Segui <sergionicolassegui@mac.lan>
2024-10-15 23:49:13 +0100

    Initial alphabet class

--- src/components/Alphabet.java
+++ src/components/Alphabet.java
@@ -1,5 +1,18 @@
 package src.components;
 
-public class Alphabet {
+import java.util.Set;
+import java.util.Vector;
 
+public class Alphabet {
+  private Set<State> alphabet;
+
+  public Alphabet() {}
+  public Alphabet(Vector<State> rawAlphabet) {
+    for (State state : rawAlphabet) {
+      this.alphabet.add(state);
+    }
+  }
+  public void add(State state) {
+    this.alphabet.add(state);
+  }
 }
